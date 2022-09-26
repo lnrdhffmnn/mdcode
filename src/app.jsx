bc610d343d6036217e98b5c9d722f73576a33f7b
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-09-26 13:08:08 -0300

    Add initial Markdown support

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
 import "./app.css";
+import CodeMirror from "@uiw/react-codemirror";
+import ReactMarkdown from "react-markdown";
+import { useState } from "react";
+import { langs } from "@uiw/codemirror-extensions-langs";
 
 export default function App() {
+  const [md, setMd] = useState("# Hello World");
+
   return (
     <div className="app">
-      <h1>Olá mundo</h1>
+      <CodeMirror
+        value={md}
+        height="100%"
+        extensions={[langs.markdown()]}
+        onChange={setMd}
+        basicSetup={{
+          crosshairCursor: false,
+        }}
+      />
+      <div className="md">
+        <ReactMarkdown>{md}</ReactMarkdown>
+      </div>
     </div>
   );
 }
